cd5cadfbd16387a94f432032a9cbd9d1bde8a0ad
Mongjin <30318269+Mongjin@users.noreply.github.com>
2023-11-19 17:46:01 +0900

    add instruction to llama2_selfInstruct_dial_aug.py

--- llama2_selfInstruct_dial_aug.py
+++ llama2_selfInstruct_dial_aug.py
@@ -9,6 +9,7 @@ from peft import AutoPeftModelForCausalLM
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from transformers import TrainingArguments
 import json
+import random
 
 model_id = "NousResearch/Llama-2-7b-chat-hf" # non gated with RLHF version
 
@@ -34,20 +35,27 @@ with open('samples_translation.json', 'r', encoding='utf-8') as fr:
     for line in fr.readlines():
         datas.append(json.loads(line))
 
-print(len(datas))
-# prompt = f"""### Instruction:
-# You are a chatbot that responds to user queries for recommendations while considering the dialogue state. The dialogue state should be appropriately generated based on the given conversation. If I provide the entire conversation, create the dialogue state up to the last turn in a key-value format after [Output State]. Additionally, to make a more suitable recommendation, create the required additional dialogue state in a key-value format after [Output State] as well. Finally, after [Output Response], generate an appropriate response to the last user utterance.
-#
-# ### Input:
-# [State] None [Dialogue] user: I'm thinking about what to eat for dinner later.
-#
-# ### Response: [Output State]
-# """
-#
-# input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.cuda()
-# # with torch.inference_mode():
-# outputs = model.generate(input_ids=input_ids, max_new_tokens=100, do_sample=True, top_p=0.9,temperature=0.9)
-#
-# # print(f"Prompt:\n{sample['response']}\n")
-# print(f"Generated instruction:\n{tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0][len(prompt):]}")
-# # print(f"Ground truth:\n{sample['instruction']}")
+random_indies = []
+while len(random_indies) < 6:
+    index = random.randn(0, 60)
+    if index not in random_indies:
+        random_indies.append(index)
+
+
+
+prompt = f"""### Instruction:
+Please generate new dialogue that 'user' is asking about recommendation food or travel 'bot'. You can reference given samples. You should follow the structure of given samples and always finish with 'bot: '.
+
+### Input: [Sample 1] {datas[random_indies[0]]['dialogue']} [Sample 2] {datas[random_indies[1]]['dialogue']} [Sample 3] {datas[random_indies[2]]['dialogue']} [Sample 4] {datas[random_indies[3]]['dialogue']} [Sample 5] {datas[random_indies[4]]['dialogue']} [Sample 6] {datas[random_indies[5]]['dialogue']} 
+
+
+### Output:
+"""
+
+input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.cuda()
+# with torch.inference_mode():
+outputs = model.generate(input_ids=input_ids, max_new_tokens=100, do_sample=True, top_p=0.9,temperature=0.9)
+
+# print(f"Prompt:\n{sample['response']}\n")
+print(f"Generated instruction:\n{tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0][len(prompt):]}")
+# print(f"Ground truth:\n{sample['instruction']}")
