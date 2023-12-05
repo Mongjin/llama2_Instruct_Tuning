9050bd630c64b5676b1354ebfd8704214cbbeddd
Mongjin <30318269+Mongjin@users.noreply.github.com>
2023-12-05 15:59:25 +0900

    delete api key

---
+++ augmented_dial_diversity.py
@@ -0,0 +1,33 @@
+import json
+from tqdm import tqdm
+from transformers import AutoTokenizer
+from matplotlib import pyplot as plt
+model_id = "/workspace/Llama-2-13b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+
+def get_data(file_path):
+    datas = []
+    with open(file_path, 'r', encoding='utf-8') as fr:
+        for data in fr.readlines():
+            datas.append(json.loads(data)['dialogue'])
+    return datas
+
+
+def get_diversity(datas):
+    token_dict = {}
+    for data in datas:
+        tokens = tokenizer.tokenize(data)
+        for token in tokens:
+            if token not in token_dict:
+                token_dict[token] = 1
+            else:
+                token_dict[token] += 1
+    token_dict = sorted(token_dict.items(), reverse=True, key=lambda item: item[1])
+    return token_dict
+
+
+if __name__ == "__main__":
+    datas = get_data('./augmented_dial_gpt-4.jsonl')
+    token_dict = get_diversity(datas)
+    print(token_dict[:10])
--- chatgpt_sample.py
+++ chatgpt_sample.py
@@ -1,9 +1,16 @@
-import openai
+from openai import OpenAI
+import json
+import random
+from tqdm import tqdm
+
+api_key = input("Please enter your API KEY: ")
+API_KEY =  api_key ## 본인 API key 입력
+ENGINE = 'gpt-4-1106-preview' ## 지금 버전이 gpt-4 turbo!
+client = OpenAI(api_key=API_KEY)
 
 
-def run_gpt_turbo(api_key, engine, prompt):
-    openai.api_key = api_key
-    completion = openai.ChatCompletion.create(
+def run_gpt_turbo(engine, prompt):
+    completion = client.chat.completions.create(
         model=engine,
         messages=[{"role": "user", "content": prompt}], # 입력 prompt
         max_tokens=2048,
@@ -16,34 +23,32 @@ def run_gpt_turbo(api_key, engine, prompt):
         # stop=[],
     )
 
-    answer = completion['choices'][0]['message']['content'].strip()
-    usage = completion["usage"]
+    print(completion)
+
+    answer = completion.choices[0].message.content
+    usage = completion.usage
     
     return answer, usage
 
-def run_text_davinci(
-        api_key, engine, prompt, max_tokens, temperature, top_p, 
+def run_text_davinci(engine, prompt, max_tokens, temperature, top_p,
         frequency_penalty, presence_penalty, logprobs, n, best_of, stop_sequences=None, debug=False
     ):
     response = None
-    openai.api_key = api_key
-    
+
     try:
         prompt += "\n"
-        response = openai.Completion.create(
-            api_key=api_key,
-            engine=engine,
-            prompt=prompt,
-            max_tokens=max_tokens,
-            temperature=temperature,
-            top_p=top_p,
-            frequency_penalty=frequency_penalty,
-            presence_penalty=presence_penalty,
-            stop=stop_sequences,
-            logprobs=logprobs,
-            n=n,
-            best_of=best_of,
-        )
+        response = client.completions.create(
+                     engine=engine,
+                     prompt=prompt,
+                     max_tokens=max_tokens,
+                     temperature=temperature,
+                     top_p=top_p,
+                     frequency_penalty=frequency_penalty,
+                     presence_penalty=presence_penalty,
+                     stop=stop_sequences,
+                     logprobs=logprobs,
+                     n=n,
+                     best_of=best_of)
         if debug:
             return response["choices"][0]["text"], response["usage"]["total_tokens"], response
         return response["choices"][0]["text"], response["usage"]["total_tokens"]
@@ -53,10 +58,54 @@ def run_text_davinci(
         return None, None
 
 
-API_KEY = '' ## 본인 API key 입력
-ENGINE = 'gpt-4.0-turbo'
+def get_datas(file_path):
+    datas = []
+    with open(file_path, 'r', encoding='utf-8') as fr:
+        for line in fr.readlines():
+            datas.append(json.loads(line))
+    return datas
 
-# prompt = '''### Instruction: Please generate new dialogue that 'user' is asking recommendation food or travel for 'bot'. 'bot' should respond like dialogue agent that request more information for better recommendation rather than recommend directly. You should follow the structure of given samples; You should always start with user's utterance and finish wih "bot: ".; Lastly, you should try to avoid similiar dialogue with samples. \n ### Input: [Sample 1] {datas[random_indies[0]]['dialogue']} \n [Sample 2] {datas[random_indies[1]]['dialogue']} \n [Sample 3] {datas[random_indies[2]]['dialogue']} \n [Sample 4] {datas[random_indies[3]]['dialogue']} \n [Sample 5] {datas[random_indies[4]]['dialogue']} \n [Sample 6] {datas[random_indies[5]]['dialogue']} \n ### Output: [Sample 7] '''
-prompt = '''### Instruction: Please generate new dialogue that 'user' is asking recommendation food or travel for 'bot'. 'bot' should respond like dialogue agent that request more information for better recommendation rather than recommend directly. You should follow the structure of given samples; You should always start and finish with user's utterance; Lastly, you should try to avoid similiar dialogue with samples. \n ### Input: [Sample 1] {datas[random_indies[0]]['dialogue']} \n [Sample 2] {datas[random_indies[1]]['dialogue']} \n [Sample 3] {datas[random_indies[2]]['dialogue']} \n [Sample 4] {datas[random_indies[3]]['dialogue']} \n [Sample 5] {datas[random_indies[4]]['dialogue']} \n [Sample 6] {datas[random_indies[5]]['dialogue']} \n ### Output: [Sample 7] '''
 
-answer, usage = run_gpt_turbo(API_KEY, ENGINE, prompt='')
+seeds = get_datas('./samples_translation.json')
+augmented_dials = get_datas('./augmented_dial_gpt-4.jsonl')
+
+answers = []
+for iter in tqdm(range(500), desc=f"Completing..."):
+    rand_seeds_indies = []
+    seeds_pool = []
+    while len(rand_seeds_indies) < 4:
+        index = random.randint(0, len(seeds)-1)
+        if index not in rand_seeds_indies:
+            rand_seeds_indies.append(index)
+            seeds_pool.append(seeds[index])
+    rand_augs_indies = []
+    augs_pool = []
+    while len(rand_augs_indies) < 2:
+        index = random.randint(0, len(augmented_dials)-1)
+        if index not in rand_augs_indies:
+            rand_augs_indies.append(index)
+            augs_pool.append(augmented_dials[index])
+
+    samples_pool = seeds_pool + augs_pool
+    random.shuffle(samples_pool)
+    # for i in range(len(seeds)):
+    #     data = seeds[i]
+    #     target_dialogue = data['dialogue']
+    #     try:
+    #         bot_index = target_dialogue.rindex("bot:")
+    #         data['dialogue'] = target_dialogue[:bot_index].strip()
+    #     except:
+    #         pass
+
+
+    # 본인 프롬프트에 맞게 수정
+    prompt = f'''### Instruction: Please generate new dialogue that 'user' is asking recommendation food or travel for 'bot'. 'bot' should respond like dialogue agent that request more information for better recommendation rather than recommend directly. You should follow the structure of given samples; You should always start and finish with user's utterance; Lastly, you should try to avoid similiar dialogue with samples. \n ### Input: [Sample 1] {samples_pool[0]['dialogue']} \n [Sample 2] {samples_pool[1]['dialogue']} \n [Sample 3] {samples_pool[2]['dialogue']} \n [Sample 4] {samples_pool[3]['dialogue']} \n [Sample 5] {samples_pool[4]['dialogue']} \n [Sample 6] {samples_pool[5]['dialogue']} \n ### Output: [Sample 7] '''
+
+    answer, usage = run_gpt_turbo(ENGINE, prompt=prompt)
+    answers.append({'prev_state': "", "dialogue": answer, "cur_state": "", "response": ""})
+    augmented_dials.append({'prev_state': "", "dialogue": answer, "cur_state": "", "response": ""})
+
+with open('augmented_dial_gpt-4.jsonl', 'w', encoding='utf-8') as fw:
+    for data in answers:
+        fw.write(json.dumps(data, ensure_ascii=False))
+        fw.write("\n")
